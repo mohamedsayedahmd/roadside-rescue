c3c4d2d75af6dee9bc5f7a6103f0391ee1a3d01e
mohamedsayedahmd <mohamedsayedahmd@gmail.com>
2023-09-04 01:36:40 +0300

    basic unit test

--- backend/src/test/java/com/roadside/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/roadside/backend/BackendApplicationTests.java
@@ -3,11 +3,28 @@ package com.roadside.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+//@SpringBootTest
 class BackendApplicationTests {
-
+	Calulator underTest = new Calulator();
 	@Test
-	void contextLoads() {
-	}
+	void itShouldAddNumbers() {
+		// given
+		int numberOne = 20;
+		int numberTwo = 30;
 
+		// when
+		int result = underTest.add(numberOne,numberTwo);
+
+		// then
+		int expected = 50;
+		assertThat(result).isEqualTo(expected);
+	}
+	class Calulator{
+		int add(int a, int b) {
+			return a + b;
+		}
+	}
 }
